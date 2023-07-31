f5b5185c8fb7fc11ee436029427eccb745f245f1
Nguyen Duc Huy <huy55465@gmail.com>
2023-07-31 17:14:43 +0700

    Bỏ bớt một số câu "using" thừa, thêm footer, gắn footer vào Home. Hiện tại footer chỉ để, cho đẹp, chứ các hyperlink và các nút dẫn đến MXH chưa có tác dụng gì cả

---
+++ PhiloStory2/MVVM/View/FooterView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace PhiloStory2.MVVM.View
+{
+	/// <summary>
+	/// Interaction logic for FooterView.xaml
+	/// </summary>
+	public partial class FooterView : UserControl
+    {
+        public FooterView()
+        {
+            InitializeComponent();
+        }
+
+		
+	}
+}
--- PhiloStory2/MVVM/ViewModel/SettingViewModel.cs
+++ PhiloStory2/MVVM/ViewModel/SettingViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PhiloStory2.MVVM.ViewModel
+﻿namespace PhiloStory2.MVVM.ViewModel
 {
 	internal class SettingViewModel
 	{

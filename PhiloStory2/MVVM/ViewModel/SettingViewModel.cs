1ac86ce9242eaeefdf8dca43b56cb24d47e9efa1
D144679076-cmd <95338319+D144679076-cmd@users.noreply.github.com>
2023-08-04 10:28:57 +0700

    Update SettingViewModel.cs

--- PhiloStory2/MVVM/ViewModel/SettingViewModel.cs
+++ PhiloStory2/MVVM/ViewModel/SettingViewModel.cs
@@ -1,4 +1,4 @@
-﻿using PhiloStory2.Core;
+using PhiloStory2.Core;
 using System;
 using System.Windows;
 
@@ -6,27 +6,8 @@ namespace PhiloStory2.MVVM.ViewModel
 {
 	internal class SettingViewModel : ObservableObject
 	{
-		public FooterViewModel FooterVM { get; set; }
-
-		
-
-		private object footerView;
-
-		public object FooterView
-		{
-			get { return footerView; }
-			set { footerView = value;
-				OnPropertyChanged();
-			}
-		}
-
 		public SettingViewModel() {
-		    FooterVM = new FooterViewModel();
-			FooterView = FooterVM;
 		
 		}
-
-	
-
 	}
 }

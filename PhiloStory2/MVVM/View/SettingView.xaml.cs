d9098da582951cf8d20947082da34c85e1020629
D144679076-cmd <95338319+D144679076-cmd@users.noreply.github.com>
2023-08-04 10:26:39 +0700

    Update SettingView.xaml.cs

--- PhiloStory2/MVVM/View/SettingView.xaml.cs
+++ PhiloStory2/MVVM/View/SettingView.xaml.cs
@@ -1,5 +1,6 @@
 using PhiloStory2.Core;
 using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -19,25 +20,40 @@ namespace PhiloStory2.MVVM.View
 		private void SettingButton_MouseEnter(object sender, RoutedEventArgs e)
 		{
 			SoundEffect.PlayMouseHover();
-        	}
-
-	        private void SettingButton_MouseEnter(object sender, RoutedEventArgs e)
-			{
-				SoundEffect.PlayMouseHover();
-	        }
-	
-	        private void LightMode(object sender, RoutedEventArgs e)
-	        {
-				SwitchTheme.ChangeTheme(new Uri("Theme/Light.xaml", UriKind.Relative));
-	        }
-	
-	        private void NeutralMode(object sender, RoutedEventArgs e)
-	        {
-	            SwitchTheme.ChangeTheme(new Uri("Theme/NeutralTheme.xaml", UriKind.Relative));
-	        }
-	
-	        private void DarkMode(object sender, RoutedEventArgs e)
-	        {
-	            SwitchTheme.ChangeTheme(new Uri("Theme/DarkTheme.xaml", UriKind.Relative));
         }
+
+        private void NeutralMode(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme.ChangeTheme(new Uri("Theme/NeutralTheme.xaml", UriKind.Relative));
+        }
+
+        private void DarkMode(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme.ChangeTheme(new Uri("Theme/DarkTheme.xaml", UriKind.Relative));
+        }
+
+        private void hyperAboutUs_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true }); ;
+            e.Handled = true;
+        }
+
+        private void hyperContact_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true }); ;
+            e.Handled = true;
+        }
+
+        private void hyperCourse_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true }); ;
+            e.Handled = true;
+        }
+
+        private void hyperFAQ_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true }); ;
+            e.Handled = true;
+        }
+    }
 }

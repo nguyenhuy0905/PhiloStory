9f1bb0c258bf21a5f083c433e0b288b601173c27
D144679076-cmd <95338319+D144679076-cmd@users.noreply.github.com>
2023-08-04 07:08:06 +0700

    Add files via upload

---
+++ PhiloStory2/Core/SwitchTheme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PhiloStory2.Core
+{
+    class SwitchTheme
+    {
+        public static void ChangeTheme(Uri theme)
+        {
+            ResourceDictionary Theme = new ResourceDictionary() { Source = theme };
+            App.Current.Resources.MergedDictionaries[0].Clear();
+            App.Current.Resources.MergedDictionaries.Add(Theme);
+
+        }
+    }
+}

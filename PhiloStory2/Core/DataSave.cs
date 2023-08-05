a7ada62c71d1f10630f106a2932ca996e89f83ef
D144679076-cmd <95338319+D144679076-cmd@users.noreply.github.com>
2023-08-05 22:58:37 +0700

    Add files via upload

---
+++ PhiloStory2/Core/DataSave.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PhiloStory2.Core
+{
+    internal class DataSave:ConfigurationSection
+    {
+        [ConfigurationProperty("DarkMode", DefaultValue = false)]
+        public bool DarkMode {
+            get { return (bool)this["DarkMode"]; } 
+            set { this["DarkMode"] = value; } 
+        }
+        [ConfigurationProperty("NeutralMode", DefaultValue = true)]
+        public bool NeutralMode
+        {
+            get { return (bool)this["NeutralMode"]; }
+            set { this["NeutralMode"] = value; }
+        }
+    }
+}

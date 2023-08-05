60dcc5f75e88ffc7d4f910dc7f2ae5fe78d7a9f1
D144679076-cmd <95338319+D144679076-cmd@users.noreply.github.com>
2023-08-05 23:02:40 +0700

    Update SettingView.xaml.cs

--- PhiloStory2/MVVM/View/SettingView.xaml.cs
+++ PhiloStory2/MVVM/View/SettingView.xaml.cs
@@ -1,5 +1,6 @@
 using PhiloStory2.Core;
 using System;
+using System.Configuration;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,25 +12,29 @@ namespace PhiloStory2.MVVM.View
 	/// </summary>
 	public partial class SettingView : UserControl
 	{
+        Configuration AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        public SettingView()
+	{
+		InitializeComponent();
+            	var Data_read = AppConfig.GetSection("DataSave");
+            	this.DataContext = Data_read;
+        }
 
-		public SettingView()
-		{
-			InitializeComponent();			
-		}
-
-		private void SettingButton_MouseEnter(object sender, RoutedEventArgs e)
-		{
-			SoundEffect.PlayMouseHover();
+	private void SettingButton_MouseEnter(object sender, RoutedEventArgs e)
+	{
+		SoundEffect.PlayMouseHover();
         }
 
         private void NeutralMode(object sender, RoutedEventArgs e)
         {
             SwitchTheme.ChangeTheme(new Uri("Theme/NeutralTheme.xaml", UriKind.Relative));
+            AppConfig.Save();
         }
 
         private void DarkMode(object sender, RoutedEventArgs e)
         {
             SwitchTheme.ChangeTheme(new Uri("Theme/DarkTheme.xaml", UriKind.Relative));
+            AppConfig.Save();
         }
 
         private void hyperAboutUs_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
